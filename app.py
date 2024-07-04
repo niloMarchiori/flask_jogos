7abf70317f909819debe3483dd35a229767c2d8f
Nilo Marchiori <nilo.marchiori@gmail.com>
2024-07-04 15:05:19 -0300

    implementação de login

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask,render_template, request, redirect, session
 from models.jogos import *
 
-app=Flask(__name__)
 cs=Jogo('CS','FPS','PC')
 valorant=Jogo('Valorant','FPS','PC')
+
+app=Flask(__name__)
+app.secret_key='alura'
+
 
 @app.route('/')
 def index():
@@ -11,7 +14,6 @@ def index():
     return render_template('lista_jogos.html', titulo='Jogos',jogos=jogos)
 
 @app.route('/novo')
-
 def novo():
     return render_template('novo.html', titulo='Novo jogo')
 
@@ -21,8 +23,21 @@ def criar():
     categoria=request.form['categoria']
     plataforma=request.form['plataforma']
     jogo=Jogo(nome,categoria, plataforma)
-
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    if 'Senha'==request.form['senha']:
+        session['usuario_logado']=request.form['usuario']
+        flash('Usuario logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
 
 if __name__=='__main__':
     app.run()
